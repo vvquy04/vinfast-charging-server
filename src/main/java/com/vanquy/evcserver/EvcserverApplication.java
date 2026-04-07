60bcc263f66c15aff14533307d6348c429a0086f
Van Quy <vivanquy20804@gmail.com>
2026-04-08 00:49:19 +0700

    chore: initial project setup

---
+++ src/main/java/com/vanquy/evcserver/EvcserverApplication.java
@@ -0,0 +1,13 @@
+package com.vanquy.evcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvcserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvcserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanquy/evcserver/EvcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanquy.evcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvcserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
