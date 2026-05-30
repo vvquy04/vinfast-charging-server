fbb347a45e6df1fa71791f54b61a79ab9bdd78b1
Van Quy <vivanquy20804@gmail.com>
2026-05-30 17:15:57 +0700

    chore(server): update pom.xml dependencies and application tests config

--- src/test/java/com/vanquy/evcserver/EvcserverApplicationTests.java
+++ src/test/java/com/vanquy/evcserver/EvcserverApplicationTests.java
@@ -2,8 +2,10 @@ package com.vanquy.evcserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EvcserverApplicationTests {
 
 	@Test
